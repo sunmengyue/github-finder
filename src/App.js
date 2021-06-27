ff3e872b73b6ac8eb17382a4d089a2963fd315d9
bardrabbit <bardrabbit@gmail.com>
2021-06-27 00:27:05 -0400

    intro to expression and condition in jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Fragment, Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  foo = () => {
+    return 'bar';
+  };
+
+  render() {
+    const name = 'Jone Doe';
+    const showName = false;
+    return (
+      <div className="App">
+        <h1>Hello {this.foo()}</h1>
+        <h2>{showName && name}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
